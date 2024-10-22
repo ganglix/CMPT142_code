c2ab80d0fbbb59505cf09ceb44f2978424f83690
Gang <ganglix@gmail.com>
2024-10-22 00:27:07 -0600

    week 1 prep

---
+++ week_1/ch03__intro.py
@@ -0,0 +1,19 @@
+# data types: int, float, bool, str, list, tuple, dict, array
+#
+# int
+
+# string
+
+# list, collection of data, compound datatype
+
+
+# boolean value
+
+
+
+# variable
+
+# first (out of three) difficult thing:
+
+
+
---
+++ week_1/ch04__intro.py
@@ -0,0 +1,23 @@
+# Indexing and Slicing of !Sequences!
+# str
+# list
+# tuple
+
+# •What is indexing used for?
+# •What does a positive index mean?
+# •What does a negative index mean?
+
+
+
+
+
+# --------- take a break -----------
+
+# •What is slicing used for?
+# •What are the meanings of the First, second, and third
+# numbers in a slicing operation?
+
+
+
+
+# I want to mention omitted value, index out of range, and range of slice
