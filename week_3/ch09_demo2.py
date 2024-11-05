fa09d61786d1462dd8af8cb9d2d64a83c75ea501
Gang <ganglix@gmail.com>
2024-11-05 00:23:33 -0600

    week 3 prep

---
+++ week_3/ch09_demo2.py
@@ -0,0 +1,9 @@
+# while vs. for
+# In this demonstration, we’ll see a situation where using a
+# while-loop is preferable to using a for-loop.
+
+# password example
+
+
+
+
---
+++ week_3/ch10___intro.py
@@ -0,0 +1,50 @@
+# lists and tuples
+
+# create a list
+
+
+# indexing and slicing, same as string, or other sequences
+
+
+# methods
+# add an item: .append(), .extend(), +
+
+
+
+
+# remove an item: .remove(), .pop(), del
+
+
+
+# locating an item: index()
+
+
+
+
+# sorting: .sort()
+
+
+
+
+
+# let do an attempt to use .sort() without changing the old L
+
+
+
+
+
+
+
+
+# list is mutable ! more examples in flowtrace observations
+
+
+
+
+# flowtrace
+
+# L = [1, 2, 3, 4, 5, 6]
+# for value in L:
+#     L.remove(value)
+# print(L)
+
